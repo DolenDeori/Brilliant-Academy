2c51ab162f12a1e11afdb531afd575784d8065cc
DolenDeori <dulench.deori564@gmail.com>
2025-02-03 00:47:19 +0530

    feat: configure PostgreSQL database settings and load environment variables

--- backend/brillant/settings.py
+++ backend/brillant/settings.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'api',  
+    'api',
     'home',
     "accounts"
 ]
@@ -74,11 +78,24 @@ WSGI_APPLICATION = 'brillant.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+database_url = os.getenv("DATABASE_URL", "")
 
+if not database_url:
+    raise ValueError("DATABASE_URL environment variable is not set!")
+
+tmpPostgres = urlparse(database_url)
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.lstrip('/'),  # Remove leading '/' properly
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': tmpPostgres.port or 5432,  # Use default port if not provided
     }
 }
 
