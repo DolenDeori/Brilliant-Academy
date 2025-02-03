9fb25a63260c844135c59613eddc87c064bb31b1
DolenDeori <dulench.deori564@gmail.com>
2025-02-03 15:19:49 +0530

    feat: add About and Contact pages with Navigation component

---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <>
+      <Navigation />
+      <div className="h-svh w-full flex justify-center items-center bg-amber-500 text-white text-3xl">
+        <h1>About</h1>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+const Contact = () => {
+  return (
+    <>
+      <Navigation />
+      <div>Contact</div>
+    </>
+  );
+};
+
+export default Contact;
